d27ec2cf93a115196efe4dccf1e7ac49bebced92
Jari Voutilainen <jari.voutilainen@iki.fi>
2012-05-15 09:59:23 +0300

    Make logging asynchronous

--- Request-Visualizer.js
+++ Request-Visualizer.js
@@ -2,9 +2,11 @@ var geoip = require('geoip-lite');
 
 module.exports = function(req, res, next ){
 	
-	var ip = req.header('x-forwarded-for');
-	console.log(ip);
-	var geo = geoip.lookup(ip);
-	console.log(geo);
+	process.nextTick(function(){
+		var ip = req.header('x-forwarded-for');
+		console.log(ip);
+		var geo = geoip.lookup(ip);
+		console.log(geo);
+	});
 	next();
 }
